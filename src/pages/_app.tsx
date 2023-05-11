f4a5b0f77b4d512fdcaa10dea824352e314f2ca3
Yura <jormungand1616@gmail.com>
2023-05-11 03:25:38 +0300

    add fonts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import '@/styles/general.scss'
+import '@/styles/fonts.scss'
 import type {AppProps} from 'next/app'
 import {Layout} from "@/components/Layout/layout";
 
