3c2084a932b8ca7e3420e30a9fe7c1cd9c09f053
Yura <jormungand1616@gmail.com>
2023-05-12 14:44:27 +0300

    some fix

---
+++ src/pages/types.ts
@@ -0,0 +1,18 @@
+export type VacancyType = {
+  id: number;
+  profession: string;
+  payment_from: number;
+  currency: string;
+  schedule: string;
+  location: string;
+};
+
+export type CatalogType = { label: string; isHidden?: boolean; key?: number };
+
+export type FiltersType = {
+  department: CatalogType;
+  salaryFrom: string | number;
+  salaryTo: string | number;
+};
+
+export type VacancyWithDescriptionType = VacancyType & { description: string };
