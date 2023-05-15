157878376e19bb79fc111be3488b561fa2b5aaef
Yura <jormungand1616@gmail.com>
2023-05-15 17:44:55 +0300

    add Authorization header

--- src/Http/vacancies.ts
+++ src/Http/vacancies.ts
@@ -39,14 +39,13 @@ export const VacancyService = {
     if (query.page) {
       queryString.push(`page=${query.page - 1}`);
     }
-    const accessToken = localStorage.getItem('access_token');
+    const access_token = localStorage.getItem('access_token');
 
     const response = await $api(
       `/vacancies/?${queryString.join('&')}&count=${itemsPerPage}&published=1`,
       {
         headers: {
-          'x-secret-key': 'GEU4nvd3rej*jeh.eqp',
-          access_token: accessToken,
+          Authorization: `Bearer ${access_token}`,
           'x-api-app-id':
             'v3.r.137528818.42ccfb7041ed0160dabfa6a739a631448f0ff724.49cb285f6936b5889d7540c35b5edf9f2d427a0a',
           client_id: '2388',
@@ -55,25 +54,12 @@ export const VacancyService = {
     );
     return response;
   },
-  async getAllVacancies() {
-    const response = await $api(`/vacancies/?catalogues=${defaultCategory}`, {
-      headers: {
-        'x-secret-key': 'GEU4nvd3rej*jeh.eqp',
-        access_token:
-          'v3.r.137440105.041d2e6c58c1314e6a2157b49200cf2f1665835e.4838e8f3fc75f96636e9c206e8fbfc1cd8c15dc8',
-        'x-api-app-id':
-          'v3.r.137528818.42ccfb7041ed0160dabfa6a739a631448f0ff724.49cb285f6936b5889d7540c35b5edf9f2d427a0a',
-        client_id: '2388',
-      },
-    });
-    return response;
-  },
   async getVacancyById(id: number) {
-    const accessToken = localStorage.getItem('access_token');
+    const access_token = localStorage.getItem('access_token');
     const response = await $api(`/vacancies/${id}`, {
       headers: {
-        'x-secret-key': 'GEU4nvd3rej*jeh.eqp',
-        access_token: accessToken,
+        Authorization: `Bearer ${access_token}`,
+        access_token,
         'x-api-app-id':
           'v3.r.137528818.42ccfb7041ed0160dabfa6a739a631448f0ff724.49cb285f6936b5889d7540c35b5edf9f2d427a0a',
         client_id: '2388',
@@ -83,26 +69,7 @@ export const VacancyService = {
   },
 
   async getCatalogues() {
-    const response = await $api(`/catalogues/parent/${defaultCategory}`, {
-      maxRedirects: 2,
-    });
-    return response;
-  },
-
-  async getFavoriteVacancies(ids: Array<number>) {
-    const queryParams = ids.map(
-      (id, index) => `ids[${index}]=${id}${index !== ids.length - 1 ? '&' : ''}`
-    );
-    const response = await $api(`/vacancies/?ids=${queryParams.join('')}`, {
-      headers: {
-        'x-secret-key': 'GEU4nvd3rej*jeh.eqp',
-        access_token:
-          'v3.r.137440105.041d2e6c58c1314e6a2157b49200cf2f1665835e.4838e8f3fc75f96636e9c206e8fbfc1cd8c15dc8',
-        'x-api-app-id':
-          'v3.r.137528818.42ccfb7041ed0160dabfa6a739a631448f0ff724.49cb285f6936b5889d7540c35b5edf9f2d427a0a',
-        client_id: '2388',
-      },
-    });
+    const response = await $api(`/catalogues/parent/${defaultCategory}`);
     return response;
   },
 };
